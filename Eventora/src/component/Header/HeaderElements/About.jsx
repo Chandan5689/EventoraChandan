45dfd28ca1eb91be357ef5b086a4088ed4728632
Aniket <aniketadhikari2@gmail.com>
2024-12-04 20:31:31 +0545

    created HeaderElements folder and created aboutus page in it

---
+++ Eventora/src/component/Header/HeaderElements/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function About() {
+  return (
+    <div style={{height:"70vh"}} className='bg-blue-600 h-4/6'>About
+    <h1>Im about page hai guyss</h1>
+
+    </div>
+
+  )
+}
+
+export default About
