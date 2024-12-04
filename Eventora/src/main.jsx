f611677d4156e5f99a97ff49ca65f102cca6297d
Aniket <aniketadhikari2@gmail.com>
2024-12-04 20:33:03 +0545

    Installed react router in package.json and created routing in main.jsx

--- Eventora/src/main.jsx
+++ Eventora/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Layout from './Layout.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes,  } from 'react-router-dom'
 import App from './App.jsx'
+import About from './component/Header/HeaderElements/About.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>} >
+      <Route path='' element={<App/>} />
+      <Route path='aboutus' element={<About/>} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </StrictMode>,
 )
