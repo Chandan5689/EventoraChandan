5c3cf43823bcb7254743235208279302640aae78
Aniket <aniketadhikari2@gmail.com>
2024-12-05 12:26:58 +0545

    added profile page

---
+++ Eventora/src/component/Profile/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <div className='flex h-screen'>
+        <div className='bg-gray-500 w-1/4'>
+        <h1 className='text-2xl '>Eventora</h1>
+        </div>
+        <div className='w-3/4'>Right side</div>
+    </div>
+  )
+}
+
+export default Profile
