ec65fef34f7fff95fc96b505dcdd288599511fd9
Aniket <aniketadhikari2@gmail.com>
2024-12-05 12:11:52 +0545

    created routing for add event, about us

--- Eventora/src/main.jsx
+++ Eventora/src/main.jsx
@@ -5,12 +5,14 @@ import Layout from './Layout.jsx'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes,  } from 'react-router-dom'
 import App from './App.jsx'
 import About from './component/Header/HeaderElements/About.jsx'
+import AddEvents from './component/Header/HeaderElements/AddEvents.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout/>} >
       <Route path='' element={<App/>} />
       <Route path='aboutus' element={<About/>} />
+      <Route path='addevent' element={<AddEvents/>} />
     </Route>
   )
 )
