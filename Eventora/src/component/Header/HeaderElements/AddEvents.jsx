c5d1d35bd679c2e520b6a01354567e0aa3de825c
Aniket <aniketadhikari2@gmail.com>
2024-12-05 12:09:25 +0545

    added link tag for routing for add event button

---
+++ Eventora/src/component/Header/HeaderElements/AddEvents.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AddEvents() {
+  return (
+    <div style={{height:"70vh"}} className='bg-blue-600 h-4/6'>
+    <h1>Im about Add Events page guyss</h1>
+
+    </div>
+  )
+}
+
+export default AddEvents
