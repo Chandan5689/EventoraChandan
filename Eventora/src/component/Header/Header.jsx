a130cb76cc0974dcf55c2390afbd37d14412ace4
Aniket <aniketadhikari2@gmail.com>
2024-12-06 20:40:13 +0545

    created a drop down for profile and removed unnecessary button usage on profile

--- Eventora/src/component/Header/Header.jsx
+++ Eventora/src/component/Header/Header.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import profileImage from '../../assets/profile.png'
 import { Link, NavLink } from 'react-router-dom'
+import Dropdown from '../Dropdown/Dropdown'
+import { useState } from 'react'
 
 function Header() {
+  
+  const [showmenu, setsShowmenu] = useState(false);
+  const openMenu = ()=>{
+    setsShowmenu(!showmenu)
+  }
+  
   return (
     <div className='flex h-32 w-screen justify-between pt-4 align-middle '>
         <Link to='/'><h1 className='pl-4 text-2xl'>Eventora</h1></Link>
@@ -13,7 +21,12 @@ function Header() {
                 <li><NavLink to='/addevent' className={({isActive})=> `${isActive? "text-red-600" : "text-black"} hover:text-red-600`}>Add Event</NavLink></li>
             </ul>
         </div>
-        <div className='mr-5'><Link to='/profile'><img className='h-10' src={profileImage} alt="" /></Link></div>
+        <div className='cursor-pointer w-48 flex flex-col justify-start items-center ' onClick={openMenu}>
+        <img className='h-10 ' src={profileImage} alt="" />
+       <div>{!showmenu ? "" : <Dropdown/>}</div> 
+       
+        
+        </div>
     </div>
   )
 }
