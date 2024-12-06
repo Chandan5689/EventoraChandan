376443ea0b965388287c17ea0600d01c3b772bcd
Aniket <aniketadhikari2@gmail.com>
2024-12-06 20:40:46 +0545

    Added dimension on box

---
+++ Eventora/src/component/Dropdown/Dropdown.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dropdown() {
+  return (
+    <div className='border-2 border-black h-52 w-48'>
+        hello everybody im just a drop down
+    </div>
+  )
+}
+
+export default Dropdown
